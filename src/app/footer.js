a3cebdcad173e3959df0e9eb04c7338be78ce2b1
Hongxin Zhang <jackson.zhang.828@gmail.com>
2016-07-27 18:14:12 -0400

    Add header/footer example and read global properties from JSON file

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,8 +1,30 @@
 import {Component} from '@angular/core';
+import {Http, HTTP_PROVIDERS} from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'Footer',
   moduleId: __moduleName,
-  templateUrl: 'footer.html'
+  templateUrl: 'footer.html',
+  providers: [HTTP_PROVIDERS]
 })
-export class Footer {}
+
+
+export class Footer {
+  constructor(http) {
+    this.http = http;
+    this.getProps().subscribe(result => {
+      this.version = result.version;
+    });
+  }
+
+  static get parameters() { // http://stackoverflow.com/questions/34800150/angular-2-es6-inject-http
+    return [[Http]];
+  }
+
+  getProps() {
+    return this.http
+        .get('src/data/global.json')
+        .map(response => response.json());
+  }
+}
